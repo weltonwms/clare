48f98c0ca04b00d9b935c7b4518d82a71e78f012
welton <welton@welton-pc>
2017-12-27 11:02:55 -0200

    inclusão total pago em impressao servico

--- application/views/servico/impressao_servico.php
+++ application/views/servico/impressao_servico.php
@@ -4,8 +4,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Impressão de Serviço</title>
         <style>
-            
-            
+
+
             div{
                 padding: 2px;
                 margin: 8px;
@@ -20,14 +20,14 @@
                 padding-left: 5px;
             }
             .container{
-               
+
                 width:100%;
-               padding: 20px;
+                padding: 20px;
 
             }
 
             #logo{
-                
+
 
                 /*max-width: 40%;*/
             }
@@ -36,37 +36,37 @@
                 border: 2px solid;
                 font-size: 14px;
                 /*width: 240px;*/
-           }
+            }
 
             #info_cliente{
-               
+
                 line-height: 25px;
                 padding:9px;
             }
 
             #discriminacao{
-               
-               /* border: 2px solid;*/
+
+                /* border: 2px solid;*/
             }
 
             #endereco{
-                
+
                 padding-top: 30px;
                 margin-top: 20px;
                 font-size: 11px;
-                
+
             }
-            
+
             #endereco td{
                 text-align: center;
-                
+
             }
             .borda{
                 border: 1px solid;
             }
 
             .coluna1_tipo_servico{
-                
+
             }
 
             .coluna2_info_cliente{
@@ -74,26 +74,26 @@
             }
 
             .borda_topo{
-               border-top: 1px solid;
-              
+                border-top: 1px solid;
+
             }
 
             .destaque{
                 background-color:  #E5E5DB;
             }
 
-           
-            
+
+
             #discriminacao table #preco_total td:first-child{
                 border-left:none ;
                 border-bottom: none;
                 font-weight: bold;
             }
-            
-             #discriminacao table #preco_total td:last-child{
-                
+
+            #discriminacao table #preco_total td:last-child{
+
             }
-            
+
             #discriminacao table td, #discriminacao table th{
                 border: 1px solid;
             }
@@ -102,8 +102,11 @@
     <body>
         <?php
         $base_url = FCPATH;
-        
+        $total_pago = $servico->get_soma_pagamentos();
+        $restante_pagar = $servico->get_total_geral_venda() - $total_pago;
         ?>
+
+        
 
 
         <div class="container">
@@ -120,19 +123,19 @@
                             <table >
                                 <tr >
                                     <td  class="coluna1_tipo_servico"> Nº Serviço </td>
-                                    <td><?php echo $servico->get_id_servico()?></td>
+                                    <td><?php echo $servico->get_id_servico() ?></td>
                                 </tr>
                                 <tr>
-                                    <td> <div class="borda quadradinho"><?php if ($servico->get_estado()=='2') echo "X"?>&nbsp;</div> </td>
+                                    <td> <div class="borda quadradinho"><?php if ($servico->get_estado() == '2') echo "X" ?>&nbsp;</div> </td>
                                     <td>Serviço Executado</td>
                                 </tr>
                                 <tr>
-                                    <td> <div class="borda quadradinho"><?php if ($servico->get_estado()=='1') echo "X"?>&nbsp;</div> </td>
+                                    <td> <div class="borda quadradinho"><?php if ($servico->get_estado() == '1') echo "X" ?>&nbsp;</div> </td>
                                     <td>Orçamento</td>
                                 </tr>
                                 <tr>
                                     <td>Data </td>
-                                    <td><?php echo $servico->get_data()?> </td>
+                                    <td><?php echo $servico->get_data() ?> </td>
                                 </tr>
                             </table>
                         </div>
@@ -149,23 +152,23 @@
                 <table>
                     <tr>
                         <td>Empresa:</td>
-                        <td width='200' class="coluna2_info_cliente"><?php echo $servico->get_nome_cliente()?></td>
-                        <td colspan="2">Responsável: <?php echo $servico->get_responsavel_cliente()?>&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                        
+                        <td width='200' class="coluna2_info_cliente"><?php echo $servico->get_nome_cliente() ?></td>
+                        <td colspan="2">Responsável: <?php echo $servico->get_responsavel_cliente() ?>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
                     </tr>
                     <tr>
                         <td>Endereço:</td>
-                        <td colspan="3"><?php echo $servico->get_endereco_cliente()?></td>
+                        <td colspan="3"><?php echo $servico->get_endereco_cliente() ?></td>
                     </tr>
                     <tr>
                         <td>Telefone:</td>
-                        <td colspan="3"><?php echo $servico->get_telefone_cliente()?></td>
-                     
+                        <td colspan="3"><?php echo $servico->get_telefone_cliente() ?></td>
+
                     </tr>
                     <tr>
-                        <td>Pagamento:</td>
-                        <td colspan="3"> <?php echo $servico->get_forma_pagamento()?></td>
-                        
+                        <td colspan="4"> Valor Pago: <?php echo number_format($total_pago, 2, ",", "."); ?> | Restante: <?php echo number_format($restante_pagar, 2, ",", ".") ?></td>
+
+
                     </tr>
                 </table>
             </div>
@@ -178,29 +181,31 @@
                         <th width="15%">Preço Un</th>
                         <th width="15%">Total</th>
                     </tr>
-                   <?php $cont=1;foreach($servico->get_itens_servico() as $item): ?>
-                    <tr>
-                        <td><?php echo $cont ?> </td>
-                        <td><?php echo $item->get_qtd_produto() ?></td>
-                        <td>
-                            <?php echo $item->get_nome_produto()?>
-                             <?php echo"<br>". $item->get_descricao()?>
-                        </td>
-                         <td><?php echo "R$ " . formatar_float_to_money($item->get_valor_unitario_venda()); ?></td>
-                         <td><?php echo "R$ " . number_format($item->get_total_venda(), 2, ",", "."); ?></td>
-                    </tr>
-                    <?php $cont++; endforeach;?>
-                    <?php if($imprimir_total):?>
-                    <tr id="preco_total">
-                        <td colspan="4" align="right"> Total Geral</td>
-                        <td class="destaque"><?php echo "R$ " . number_format($servico->get_total_geral_venda(), 2, ",", "."); ?></td>
-                    </tr>
-                    <?php endif;?>
+                    <?php $cont = 1;
+                    foreach ($servico->get_itens_servico() as $item): ?>
+                        <tr>
+                            <td><?php echo $cont ?> </td>
+                            <td><?php echo $item->get_qtd_produto() ?></td>
+                            <td>
+                                <?php echo $item->get_nome_produto() ?>
+                                <?php echo"<br>" . $item->get_descricao() ?>
+                            </td>
+                            <td><?php echo "R$ " . formatar_float_to_money($item->get_valor_unitario_venda()); ?></td>
+                            <td><?php echo "R$ " . number_format($item->get_total_venda(), 2, ",", "."); ?></td>
+                        </tr>
+                        <?php $cont++;
+                    endforeach; ?>
+<?php if ($imprimir_total): ?>
+                        <tr id="preco_total">
+                            <td colspan="4" align="right"> Total Geral</td>
+                            <td class="destaque"><?php echo "R$ " . number_format($servico->get_total_geral_venda(), 2, ",", "."); ?></td>
+                        </tr>
+<?php endif; ?>
                 </table>
                 <br>
-             <?php if($servico->get_obs()) echo "Obs: ".$servico->get_obs()?>
+<?php if ($servico->get_obs()) echo "Obs: " . $servico->get_obs() ?>
             </div>
-             
+
             <div id="endereco">
                 <!--<h4>Valparaíso de Goiás <span class='data-final'> ______/_______/_______</span></h4>--><br>
                 <table  width="100%" cellspacing='0' cellpadding='0'>
@@ -212,8 +217,8 @@
 
 
             </div>
-            
-           
+
+
         </div>
 
 
